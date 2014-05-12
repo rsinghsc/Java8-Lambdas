be3bb8c80e6506b849db60205916b480b7a7d435
Raj Singh <rsingh@sharecare.com>
2014-05-12 14:11:44 -0400

    build athlete records

---
+++ src/java8/AthleteRecord.java
@@ -0,0 +1,105 @@
+package java8;
+
+import java.time.LocalDate;
+import java.util.StringJoiner;
+
+/**
+ * Created by rsingh on 5/12/14.
+ */
+public class AthleteRecord {
+    //Athlete,Age,Country,Year,Closing Ceremony Date,Sport,Gold Medals,Silver Medals,Bronze Medals,Total Medals
+    private String name;
+    private Integer age;
+    private String country;
+    private Integer year;
+    private LocalDate closingCeremony;
+    private String sport;
+    private Integer goldMedals;
+    private Integer silverMedals;
+    private Integer bronzeMedals;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        country = country;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public LocalDate getClosingCeremony() {
+        return closingCeremony;
+    }
+
+    public void setClosingCeremony(LocalDate closingCeremony) {
+        this.closingCeremony = closingCeremony;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public Integer getGoldMedals() {
+        return goldMedals;
+    }
+
+    public void setGoldMedals(int goldMedals) {
+        this.goldMedals = goldMedals;
+    }
+
+    public Integer getSilverMedals() {
+        return silverMedals;
+    }
+
+    public void setSilverMedals(int silverMedals) {
+        this.silverMedals = silverMedals;
+    }
+
+    public Integer getBronzeMedals() {
+        return bronzeMedals;
+    }
+
+    public void setBronzeMedals(int bronzeMedals) {
+        this.bronzeMedals = bronzeMedals;
+    }
+
+    public String toString() {
+        StringJoiner joiner = new StringJoiner(" ");
+        joiner.add(name);
+        joiner.add(country);
+        joiner.add(goldMedals.toString());
+        joiner.add(silverMedals.toString());
+        joiner.add(bronzeMedals.toString());
+        return joiner.toString() ;
+    }
+
+
+
+}
--- src/java8/Demo.java
+++ src/java8/Demo.java
@@ -1,25 +1,41 @@
 package java8;
 
-import au.com.bytecode.opencsv.CSVReader;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
+import au.com.bytecode.opencsv.CSVReader;
 /**
  * Created by rsingh on 4/29/14.
  */
 public class Demo {
 
     public static void main(String[] args) {
+        List<AthleteRecord> records = new ArrayList<AthleteRecord>();
         //Load up the record
+        //Athlete,Age,Country,Year,Closing Ceremony Date,Sport,Gold Medals,Silver Medals,Bronze Medals,Total Medals
         try {
             CSVReader reader = new CSVReader(new FileReader("data/OlympicAthletes_0.csv"));
-            String[] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
-                // nextLine[] is an array of values from the line
-                System.out.println(nextLine[0] + nextLine[1] + "etc...");
+            String[] line = reader.readNext();
+            while ((line = reader.readNext()) != null) {
+                AthleteRecord record = new AthleteRecord();
+                record.setName(line[0]);
+                record.setCountry(line[2]);
+                record.setGoldMedals(Integer.parseInt(line[6]));
+                record.setSilverMedals(Integer.parseInt(line[7]));
+                record.setBronzeMedals(Integer.parseInt(line[8]));
+                records.add(record);
+//                System.out.println(nextLine[0] + nextLine[1] + "etc...");
             }
+
+            for(AthleteRecord record: records) {
+                System.out.println(record);
+            }
+        } catch (NumberFormatException nfe) {
+            System.out.println("caught number format exception");
+            nfe.printStackTrace();
         } catch (FileNotFoundException fnfe) {
             System.out.println("unable to find datafile");
         } catch (IOException ioException) {
